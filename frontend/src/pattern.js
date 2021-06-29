f897e97d07f7f7a4a8cdc2215ff784f177b8a9ee
DKendle <dina.kendle@gmail.com>
2021-06-29 11:45:10 -0400

    Adds all array to collect patterns and a push

--- frontend/src/pattern.js
+++ frontend/src/pattern.js
@@ -1,4 +1,5 @@
 class Pattern {
+    static all = []
     constructor(name, difficulty, description, yarn, needle_type, needle_size, category_id){
         this.name = name
         this.difficulty = difficulty
@@ -7,5 +8,8 @@ class Pattern {
         this.needle_type = needle_type
         this.needle_size = needle_size
         this.category_id = category_id
+
+        Pattern.all.push(this)
+
     }
 }

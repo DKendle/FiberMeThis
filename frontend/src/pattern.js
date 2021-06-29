d352c6a9c9a20cbc381025cd486e58592aad9191
DKendle <dina.kendle@gmail.com>
2021-06-29 11:27:32 -0400

    Adds pattern obj constructor

---
+++ frontend/src/pattern.js
@@ -0,0 +1,11 @@
+class Pattern {
+    constructor(name, difficulty, description, yarn, needle_type, needle_size, category_id){
+        this.name = name
+        this.difficulty = difficulty
+        this.description = description
+        this.yarn = yarn
+        this.needle_type = needle_type
+        this.needle_size = needle_size
+        this.category_id = category_id
+    }
+}

ff53dd759796503ac4f9c48a1f556b45f4e26925
DKendle <dina.kendle@gmail.com>
2021-06-28 21:43:50 -0400

    Adds div containers as consts in index.js

---
+++ frontend/index.js
@@ -0,0 +1,3 @@
+const patternContainer = document.getElementById("pattern-container")
+const formContainer = document.getElementById("form-container")
+const categoryContainer = document.getElementById("category-container")

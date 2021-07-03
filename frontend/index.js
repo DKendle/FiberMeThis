2d9e927abc60cdbf1e9fb9c0c5e1d164b01157c2
DKendle <dina.kendle@gmail.com>
2021-07-03 18:10:35 -0400

    Adds const variables from index.js to respective js files. Adds category form

--- frontend/index.js
+++ frontend/index.js
@@ -3,9 +3,16 @@ const pattern_url = base_url + "/patterns"
 const category_url = base_url + "/categories"
 
 const patternContainer = document.getElementById("pattern-container")
-const formContainer = document.getElementById("form-container")
-const categoryContainer = document.getElementById("category-container")
-const categoryList = document.getElementById("category-list")
-
 
 Category.grabCategories()
+Category.renderForm()
+//Category.categoryForm.addEventListener("submit", submitCategory())
+//Category.categoryList.addEventListener("click", clickCat())
+
+
+function submitCategory(){
+}
+
+function catClick(){
+    debugger
+}
--- frontend/src/category.js
+++ frontend/src/category.js
@@ -1,10 +1,13 @@
 class Category {
     //able to access all categories created
     static all = []
-    
+    static categoryForm = document.getElementById("form-container")
+    static categoryContainer = document.getElementById("category-container")
+    static categoryList = document.getElementById("category-list")
+
+
     constructor(name){
         this.name = name 
-
         this.element = document.createElement("li")
         this.element.id = `category-${this.name}`
         Category.all.push(this)
@@ -12,25 +15,39 @@ class Category {
 
 
     //Index function, pulls all categories
-    static grabCategories(){
-        fetch(category_url)
-        .then(resp => resp.json())
-        .then(json => {
-            for(const category of json){
-                let c = new Category(category.name)
-                c.addCategoryToDOM()
-            }
-        })
-    }
+   static grabCategories(){
+       fetch(category_url)
+       .then(resp => resp.json())
+       .then(categories => {
+           for(const category of categories){
+               let c = new Category(category.name)
+               c.addCatToDOM()
+           }
+       })
+   }
 
-    categoryHTML(){
-        this.element.innerHTML += `
-            <h3>${this.name}</3>`
-        return this.element
-    }
+   categoryHTML(){
+    this.element.innerHTML +=`
+        <h3>${this.name}</h3>
     
-    addCategoryToDOM(){
-        categoryList.append(this.categoryHTML())
-    }
+    `
+    return this.element
+   }
+
+   addCatToDOM(){
+       Category.categoryList.append(this.categoryHTML())
+   }
+
+
+static renderForm(){
+    Category.categoryForm.innerHTML +=`
+    Name: <input type="text" name="name">
+    Submit: <input type="submit">
+    
+    `
+}
+
 
 }
+
+
